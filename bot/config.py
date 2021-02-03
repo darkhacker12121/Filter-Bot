cd985620bc8d804e7bd3a56722f40e36eb1e1d35
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-02-03 04:49:56 -0800

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ class Development(Config):
     OWNER_ID = 1131653685  # my telegram ID
     OWNER_NAME = "ㅤㅤㅤㅤㅤ"
     OWNER_USERNAME = "kavinduaj"  # my telegram username
-    TOKEN = "1423025003:AAHTvOTARswwuPoMwv55lXceIQvbZhUzD8A"  # my api key, as provided by the botfather
+    API_KEY = "1423025003:AAHTvOTARswwuPoMwv55lXceIQvbZhUzD8A"  # my api key, as provided by the botfather
     SQLALCHEMY_DATABASE_URI = 'postgresql://filterbot:dkkaj0123456@postgresql/postgres'  # sample db credentials
     USE_MESSAGE_DUMP = True
     SUDO_USERS = [1131653685]  # List of id's for users which have sudo access to the bot.
