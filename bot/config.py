0db2a7ede58d5dbf172fbd5b1de59a7fb3167ffa
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-02-03 03:27:43 -0800

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,7 +4,6 @@ from bot.sample_config import Config
 class Development(Config):
     OWNER_ID = 1131653685  # my telegram ID
     OWNER_USERNAME = "kavinduaj"  # my telegram username
-    OWNER_NAME = "Kavindu AJ"
     API_KEY = "1423025003:AAHxRn9YSX2N73S72QadcImmwPH_4JQhyjU"  # my api key, as provided by the botfather
     SQLALCHEMY_DATABASE_URI = 'postgresql://filterbot:dkkaj0123456@postgresql/postgres'  # sample db credentials
     USE_MESSAGE_DUMP = True
